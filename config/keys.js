67a079e978f0fa03062c91b7ead067a6371003d9
Jeff Oliver <jeffoliver3384@gmail.com>
2019-12-17 12:25:21 -0500

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://jeffoliver:cryptolytic@cryptolytic-b1kod.mongodb.net/test?retryWrites=true&w=majority"
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors);
+app.use(express.json());
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
+
+module.exports = app;
